92a60d5e593655d86d26019b9fe4e58da1457c41
GessicaCalcagno <gessicacristinacalcagno@gmail.com>
2024-04-08 18:56:58 +0200

    Add: mileston2

--- js/script.js
+++ js/script.js
@@ -9,17 +9,40 @@ console.log(number);
 for (let i = 0; i < number.length; i++) {
   console.log(number[i].innerHTML);
   let btnNumb = number[i];
+
   btnNumb.addEventListener("click", function () {
     console.log(this.innerHTML);
     result.textContent += this.textContent;
+    currentOp += this.textContent;
   });
 }
 
-//____________________
+//____________________OPERATORS_________________________
 
 const operators = document.querySelectorAll(".op");
 console.log(operators);
 
-for (let i = 0; i < operators.length; i++ ){
-    console.log(operators[i].innerHTML);
+let currentOp = "";
+let operator = "";
+
+for (let i = 0; i < operators.length; i++) {
+  console.log(operators[i].innerHTML);
+  let btnOp = operators[i];
+
+  btnOp.addEventListener("click", function () {
+    console.log(this.innerHTML);
+
+    //Se non è una stringa vuota allora posso fare le operazini
+    if (currentOp !== "") {
+      switch (this.textContent) {
+        case "+":
+        case "-":
+        case "x":
+        case "÷":
+          operator = this.textContent;
+          result.textContent = "";
+          break;
+      }
+    }
+  });
 }
